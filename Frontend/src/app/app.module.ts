8563fcc23b0a3f80839cfcf2bea1f992db8d8e7c
Miranda0101 <xiaomeng.niu@antra.com>
2023-06-08 15:18:30 -0400

    Modules and Login component init

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -4,6 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginModule } from './modules/login/login.module';
+import { RegisterModule } from './modules/register/register.module';
+import { NewsFeedModule } from './modules/news-feed/news-feed.module';
+import { ProfileModule } from './modules/profile/profile.module';
+import { SettingModule } from './modules/setting/setting.module';
+import { AdminModule } from './modules/admin/admin.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +18,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    LoginModule,
+    RegisterModule,
+    NewsFeedModule,
+    ProfileModule,
+    SettingModule,
+    AdminModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
